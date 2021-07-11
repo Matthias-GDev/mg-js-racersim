1d1552911b408574a299d6970a0aea3c807cf24d
Matthias-GDev <Matthias@Gutbrod.digital>
2021-07-11 09:03:34 +0200

    add review changes

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -1,7 +1,7 @@
 // PROVIDED CODE BELOW (LINES 1 - 80) DO NOT REMOVE
 
 // The store will hold all information needed globally
-var store = {
+let store = {
 	track_id: undefined,
 	tracks:{},
 	player_id: undefined,
@@ -91,11 +91,11 @@ async function handleCreateRace() {
 	// render starting UI
 	renderAt('#race', renderRaceStartView(store.tracks[store.track_id-1],store.players))
 
-	// TODO - Get player_id and track_id from the store
+	// Get player_id and track_id from the store
 	const player_id = store.player_id
 	const trackid = store.track_id
 
-	// const race = TODO - invoke the API call to create the race, then save the result
+	// const race = invoke the API call to create the race, then save the result
 	try
 	{
 		// TODO - update the store with the race id
@@ -108,13 +108,13 @@ async function handleCreateRace() {
 		});
 
 		// The race has been created, now start the countdown
-		// TODO - call the async function runCountdown
+		// call the async function runCountdown
 		await runCountdown()
 
-		// TODO - call the async function startRace
+		// call the async function startRace
 		await startRace(raceId)
 
-		// TODO - call the async function runRace
+		// call the async function runRace
 		await runRace(raceId)
 
 		
@@ -126,19 +126,19 @@ async function handleCreateRace() {
 
 function runRace(raceID) {
 	return new Promise(resolve => {
-		// TODO - use Javascript's built in setInterval method to get race info every 500ms
+		// use Javascript's built in setInterval method to get race info every 500ms
 		const runRaceIntervalId = setInterval(async () => {
 			await getRace(raceID)
 				.then((res) => {
 				/* 
-					TODO - if the race info statuss property is "in-progress", update the leaderboard by calling:
+					if the race info statuss property is "in-progress", update the leaderboard by calling:
 					renderAt('#leaderBoard', raceProgress(res.positions))
 				*/
 				if (res.status === 'in-progress') {
              		renderAt('#leaderBoard', raceProgress(res.positions));
 				}
 				
-				//TODO - if the race info status property is "finished", run the following:
+				// if the race info status property is "finished", run the following:
 				/* 
 					clearInterval(raceInterval) // to stop the interval from repeating
 					renderAt('#race', resultsView(res.positions)) // to render the results view
@@ -170,7 +170,7 @@ async function runCountdown() {
 		let timer = 3
 
 		return new Promise(resolve => {
-			// TODO - use Javascript's built in setInterval method to count down once per second
+			// use Javascript's built in setInterval method to count down once per second
 			const intervalId = setInterval(() => {
 
 				// run this DOM manipulation to decrement the countdown for the user
@@ -203,7 +203,7 @@ function handleSelectPodRacer(driverId) {
 	const targetCard = document.getElementById('racercard'+driverId)
 	targetCard.classList.add('racerselected')
 
-	// TODO - save the selected racer to the store
+	// save the selected racer to the store
 	updateStore({
 		player_id: parseInt(driverId)
 	});
@@ -221,7 +221,7 @@ function handleSelectTrack(trackId) {
 	const targetCard = document.getElementById('trackcard'+trackId)
 	targetCard.classList.add('selected')
 
-	// TODO - save the selected track id to the store
+	// save the selected track id to the store
 	updateStore({
 		track_id: parseInt(trackId)
 	});
@@ -230,7 +230,7 @@ function handleSelectTrack(trackId) {
 
 function handleAccelerate() {
 	console.log('accelerate button clicked')
-	// TODO - Invoke the API call to accelerate
+	// Invoke the API call to accelerate
 	accelerate(store.race_id)
 }
 
@@ -362,7 +362,7 @@ function resultsView(positions) {
 }
 
 function raceProgress(positions) {
-	let userPlayer = positions.find(e => e.id === store.player_id)
+	const userPlayer = positions.find(e => e.id === store.player_id)
 	userPlayer.driver_name += ' (you)'
 
 	positions = positions.sort((a, b) => (a.segment > b.segment) ? -1 : 1)
@@ -423,7 +423,7 @@ function defaultFetchOpts() {
 	}
 }
 
-// TODO - Make a fetch call (with error handling!) to each of the following API endpoints 
+// Make a fetch call (with error handling!) to each of the following API endpoints 
 
 function getTracks() {
 	// GET request to `${SERVER}/api/tracks`
